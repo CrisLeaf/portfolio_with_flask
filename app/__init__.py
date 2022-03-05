8b0f960cc744eb3fecd14a3978a7264c82ebcf96
cristobal_javier@hotmail.com <cristobal_javier@hotmail.com>
2022-03-05 14:26:26 -0300

    Added conecta 4  bot

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,6 @@ import os
 def create_app():
 	app = Flask(__name__)
 	
-	app.config.from_mapping(
-		SENDGRID_KEY=os.environ.get("SENDGRID_KEY")
-	)
-	
 	from . import portfolio
 	
 	app.register_blueprint(portfolio.bp)
--- app/portfolio.py
+++ app/portfolio.py
@@ -1,6 +1,4 @@
-from flask import (
-	Blueprint, render_template
-)
+from flask import Blueprint, render_template
 
 
 bp = Blueprint("portfolio", __name__, url_prefix="/")
